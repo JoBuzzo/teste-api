0b16a440542b9cbe3d4ebf3545590544910a095b
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2025-04-15 23:03:25 -0300

    feat: efetuar login. logout e recuperar usuário logado

---
+++ app/Http/Middleware/AuthAccessToken.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\AccessToken;
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthAccessToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $accessToken = $request->bearerToken();
+
+        $token = AccessToken::where('access_token', $accessToken)->firstOrFail();
+
+        $dtCriacao = Carbon::parse($token->dt_criacao);
+
+        if ($dtCriacao->addSeconds($token->expires_in) > now()) {
+            return $next($request);
+        }
+
+        return response()->json([
+            'message' => 'Token expirado.'
+        ], 401);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\V1\AuthController;
 use App\Http\Controllers\V1\CotacaoDeFreteController;
-use Illuminate\Http\Request;
+use App\Http\Middleware\AuthAccessToken;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 
 Route::group(['prefix' => 'v1'], function () {
-    Route::get('/cotacao/frete', [CotacaoDeFreteController::class, 'index']);
-    Route::get('/cotacao/rapida/{id}', [CotacaoDeFreteController::class, 'show']);
+
+    Route::group(['middleware' => AuthAccessToken::class], function () {
+        Route::get('/cotacao/frete', [CotacaoDeFreteController::class, 'index']);
+        Route::get('/cotacao/rapida/{id}', [CotacaoDeFreteController::class, 'show']);
+        Route::post('/logout', [AuthController::class, 'destroy']);
+        Route::get('/usuario', [AuthController::class, 'show']);
+    });
+
+    Route::post('/login', [AuthController::class, 'store']);
 });
